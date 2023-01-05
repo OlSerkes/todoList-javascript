bab7830e3cbfb20ce3880f48e51367cbbe254345
Oleksandra Serkes <alexsandra.serkes@gmail.com>
2023-01-05 21:49:21 +0200

    Changed app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+const items = document.querySelectorAll('.item');
+const placeholders = document.querySelectorAll('.placeholder');
+
+for (let item of items) {
+  item.addEventListener('dragstart', dragstart);
+  item.addEventListener('dragend', dragend);
+}
+
+for (const placeholder of placeholders) {
+  placeholder.addEventListener('dragover', dragover);
+  placeholder.addEventListener('dragenter', dragenter);
+  placeholder.addEventListener('dragleave', dragleave);
+  placeholder.addEventListener('drop', dragdrop);
+}
+
+function dragstart(event) {
+  event.target.classList.add('hold');
+  item = event.target;
+  setTimeout(() => event.target.classList.add('hide'), 0);
+}
+
+function dragend(event) {
+  event.target.classList.remove('hold', 'hide');
+}
+
+function dragover(event) {
+  event.preventDefault();
+}
+
+function dragenter(event) {
+  event.target.classList.add('hovered');
+}
+
+function dragleave(event) {
+  event.target.classList.remove('hovered');
+}
+
+function dragdrop(event) {
+  event.target.append(item);
+  event.target.classList.remove('hovered');
+}
